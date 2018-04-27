a62c63e9654ff83792f226af9d19feeced019c20
18055123270 <862176213@qq.com>
2018-04-27 11:36:21 +0800

    Initial Commit

---
+++ FMDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBDemo
+//
+//  Created by 孔志林 on 2018/4/27.
+//  Copyright © 2018年 KMingMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
